b41d73086372a0e528e36209353cd8e89cfef1dd
Victor Saint Hilaire <victorshp30@gmail.com>
2024-06-05 20:42:04 -0400

    Initial Commit

---
+++ Himnos/HimnosApp.swift
@@ -0,0 +1,17 @@
+//
+//  HimnosApp.swift
+//  Himnos
+//
+//  Created by Victor Saint Hilaire on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct HimnosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
